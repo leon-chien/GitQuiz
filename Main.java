11e8dd2e772ac58d40b8652131765b064ce9e831
Rehan Shah <rehans0906@gmail.com>
2023-12-07 12:39:58 -0500

    command line argu

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main
 {
     public static void main (String[]args)
-    {
+    {/* 
         Scanner scan = new Scanner(System.in);
         String line = scan.nextLine();
         int space = line.indexOf(" ");
         int c = Integer.parseInt(line.substring(0,space));
         int d = Integer.parseInt(line.substring(space+1,line.length()));
+        */
 
     Adder adder = (int a, int b ) -> 
     {
@@ -19,7 +20,7 @@ public class Main
         }
         return sum;
     };
-    int sum = adder.add(c,d);
+    int sum = adder.add(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
     System.out.println(sum);
     }
 }
