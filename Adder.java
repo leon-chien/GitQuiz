56855c953be870a58f68ae7dba6269ec1bd6db34
leon-chien <chienleonllc@gmail.com>
2023-12-07 12:23:34 -0500

    lambda expression finished

--- Adder.java
+++ Adder.java
@@ -1,5 +1,4 @@
 public interface Adder
 {
     public int add(int a, int b);
-    public void print();
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main (String[]args)
     {
-        
-    }
+        Scanner scan = new Scanner(System.in);
+        String line = scan.nextLine();
+        int space = line.indexOf(" ");
+        int c = Integer.parseInt(line.substring(0,space));
+        int d = Integer.parseInt(line.substring(space+1,line.length()));
 
-    
+    Adder adder = (int a, int b ) -> 
+    {
+        int sum = 0; 
+        for(int i = a; i <= b; i++)
+        {
+            sum+=i;
+        }
+        return sum;
+    };
+    int sum = adder.add(c,d);
+    System.out.println(sum);
+    }
 }
